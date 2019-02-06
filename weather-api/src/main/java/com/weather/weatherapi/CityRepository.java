ca2d4d187500b03f30462e499f035a351d8a2e15
User-Komputer\User <klemst2@wp.pl>
2019-02-06 17:08:47 +0100

    fff

--- weather-api/src/main/java/com/weather/weatherapi/CityRepository.java
+++ weather-api/src/main/java/com/weather/weatherapi/CityRepository.java
@@ -1,10 +1,11 @@
 package com.weather.weatherapi;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
-	City findByName(String name);
+	List<City> findByName(String name);
 }
