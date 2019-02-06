6b63f082d6e55391c3287d1bc6f8b2218eb6869e
User-Komputer\User <klemst2@wp.pl>
2019-02-06 15:35:07 +0100

    gui

---
+++ weather-api/src/main/java/com/weather/weatherapi/UserGui.java
@@ -0,0 +1,16 @@
+package com.weather.weatherapi;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class UserGui extends VerticalLayout{
+
+//	@GetMapping("/GetCity/{cityName}")
+//	public String getCity(@PathVariable String cityName) {
+//
+//		return null;
+//
+//	}
+
+}
