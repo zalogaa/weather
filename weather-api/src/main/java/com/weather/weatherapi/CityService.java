8f1e7a6dcad32c69e9a5c960f3566bc143cc3af9
User-Komputer\User <klemst2@wp.pl>
2019-02-06 17:08:44 +0100

    fdss

--- weather-api/src/main/java/com/weather/weatherapi/CityService.java
+++ weather-api/src/main/java/com/weather/weatherapi/CityService.java
@@ -1,5 +1,7 @@
 package com.weather.weatherapi;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,7 @@ public class CityService {
 		cityRepository.save(city);
 	}
 	
-	public City getCity(String name) {
-		return cityRepository.findByName(name);
+	public List<City> getCity(String name) {
+		return (List<City>) cityRepository.findByName(name);
 	}
 }
