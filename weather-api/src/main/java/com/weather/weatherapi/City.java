80cbfb1058e2dd867c7f9e27c8166e6c0e2622ae
eviljajo <eviljajo@gmail.com>
2019-02-06 15:51:03 +0100

    City.java + repo

---
+++ weather-api/src/main/java/com/weather/weatherapi/City.java
@@ -0,0 +1,27 @@
+package com.weather.weatherapi;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cities")
+public class City {
+
+	@Column(name = "cityName", nullable = false)
+	private String cityName;
+
+	public City(String cityName) {
+
+		this.cityName = cityName;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+}
---
+++ weather-api/src/main/java/com/weather/weatherapi/cityRepository.java
@@ -0,0 +1,9 @@
+package com.weather.weatherapi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+
+
+public class cityRepository extends JpaRepositoryExtension {
+
+}
