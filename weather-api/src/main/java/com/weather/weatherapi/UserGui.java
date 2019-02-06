409c8c10a706665c2860aaa02ab9dc7b7b1a5515
User-Komputer\User <klemst2@wp.pl>
2019-02-06 16:37:48 +0100

    area

--- weather-api/src/main/java/com/weather/weatherapi/UserGui.java
+++ weather-api/src/main/java/com/weather/weatherapi/UserGui.java
@@ -1,9 +1,11 @@
 package com.weather.weatherapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -14,17 +16,28 @@ public class UserGui extends VerticalLayout {
 	private Button buttonSend;
 	@Autowired
 	private CityService cityService;
+	private TextArea taCities;
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public UserGui() {
 		this.tfCity = new TextField("city");
 		this.buttonSend = new Button("send");
-		buttonSend.addClickListener(clickEvent -> addCity(tfCity.getValue()));
+		taCities = new TextArea("cities with temperaturies");
+		buttonSend.addClickListener(clickEvent -> {
+			addCity(tfCity.getValue());
+			showCitiesWithTemperaturies();
+		});
 		add(tfCity);
 		add(buttonSend);
 	}
 
 	public void addCity(String city) {
 		cityService.addCity(new City(city));
+	}
+
+	public void showCitiesWithTemperaturies() {
+		taCities.setValue("works");
 	}
 
 }
